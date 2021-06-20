94723472494b0a2df6d4fbd85a9790ed57d0ecf5
airtonsm <airtonsmorais28@gmail.com>
2021-06-20 17:23:25 -0300

    created service product

--- src/main/java/com/airton/psjava/service/ProductService.java
+++ src/main/java/com/airton/psjava/service/ProductService.java
@@ -1,2 +1,23 @@
-package com.airton.psjava.service;public class ProductService {
+package com.airton.psjava.service;
+
+import com.airton.psjava.dto.ProductDTO;
+import com.airton.psjava.mapper.ProductMapper;
+import com.airton.psjava.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ProductService {
+
+    @Autowired
+    private ProductRepository repository;
+
+    public List<ProductDTO> findAll(String sortAttribute) {
+        String sortBy = ProductDTO.sortProduct(sortAttribute);
+        return ProductMapper.toDTOList(repository.findAll(Sort.by(Sort.Direction.ASC, sortBy)));
+    }
+
 }
