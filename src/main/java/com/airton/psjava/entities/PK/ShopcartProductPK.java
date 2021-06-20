21087d08165548d4b939737fc721d3f01aa5fd96
airtonsm <airtonsmorais28@gmail.com>
2021-06-20 13:04:57 -0300

    created composed PK class

---
+++ src/main/java/com/airton/psjava/entities/PK/ShopcartProductPK.java
@@ -0,0 +1,42 @@
+package com.airton.psjava.entities.PK;
+
+import com.airton.psjava.entities.Product;
+import com.airton.psjava.entities.Shopcart;
+
+import javax.persistence.Embeddable;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import java.io.Serializable;
+/*
+class for relation in beetwan Products and shopcart(PK composed)
+ */
+@Embeddable
+public class ShopcartProductPK implements Serializable {
+
+    @ManyToOne
+    @JoinColumn(name = "shopcart_id")
+    private Shopcart shopcart;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+    public Shopcart getShopcart() {
+        return shopcart;
+    }
+
+    public void setShopcart(Shopcart shopcart) {
+        this.shopcart = shopcart;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+
+}
+
