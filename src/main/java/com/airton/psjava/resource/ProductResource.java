b69bc27dc9b22ddfb36db882e88884629d995a68
airtonsm <airtonsmorais28@gmail.com>
2021-06-20 20:40:14 -0300

    method findById

--- src/main/java/com/airton/psjava/resource/ProductResource.java
+++ src/main/java/com/airton/psjava/resource/ProductResource.java
@@ -5,10 +5,7 @@ import com.airton.psjava.service.ProductService;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -23,6 +20,12 @@ public class ProductResource {
     public ResponseEntity<List<ProductDTO>> findAll(@RequestParam(required = false) String sortAttribute) {
         List<ProductDTO> list = service.findAll(StringUtils.upperCase(sortAttribute));
         return ResponseEntity.ok().body(list);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<ProductDTO> findById(@PathVariable Long id) {
+        ProductDTO obj = service.findById(id);
+        return ResponseEntity.ok().body(obj);
     }
 
 }
