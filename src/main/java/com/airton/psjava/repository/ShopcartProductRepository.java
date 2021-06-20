3f76ea796b006b375e056be189407083f8a4654e
airtonsm <airtonsmorais28@gmail.com>
2021-06-20 16:26:29 -0300

    repository ShopcartsProducts

--- src/main/java/com/airton/psjava/repository/ShopcartProductRepository.java
+++ src/main/java/com/airton/psjava/repository/ShopcartProductRepository.java
@@ -1,2 +1,9 @@
-package com.airton.psjava.repository;public interface ShopcartProductRepository {
+package com.airton.psjava.repository;
+
+import com.airton.psjava.entities.ShopcartProduct;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ShopcartProductRepository extends JpaRepository<ShopcartProduct, Long> {
 }
