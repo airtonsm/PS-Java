a47301b8935f9ce05228be7a42d7c4c81881c481
airtonsm <airtonsmorais28@gmail.com>
2021-06-20 20:38:22 -0300

    exception class for resource

--- src/main/java/com/airton/psjava/exception/ResourcesNotFoundException.java
+++ src/main/java/com/airton/psjava/exception/ResourcesNotFoundException.java
@@ -1,2 +1,13 @@
-package com.airton.psjava.exception;public class ResourcesNotFoundException {
+package com.airton.psjava.exception;
+
+public class ResourcesNotFoundException extends RuntimeException{
+
+    public ResourcesNotFoundException(Long id){
+        super("Resouce not found Id " + id);
+    }
+
+    public ResourcesNotFoundException(String msg){
+        super(msg);
+    }
+
 }
