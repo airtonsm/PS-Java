c1b041ea474210a6f102a909b55f23a91e52f4a3
airtonsm <airtonsmorais28@gmail.com>
2021-06-20 12:40:28 -0300

    project created

---
+++ src/main/java/com/airton/psjava/PsJavaApplication.java
@@ -0,0 +1,13 @@
+package com.airton.psjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PsJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PsJavaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/airton/psjava/PsJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.airton.psjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PsJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
