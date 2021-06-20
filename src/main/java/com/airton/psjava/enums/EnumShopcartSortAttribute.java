65c1c9e0e2e911765c57124263f70b4c9061f272
airtonsm <airtonsmorais28@gmail.com>
2021-06-20 17:11:35 -0300

    enum to order

--- src/main/java/com/airton/psjava/enums/EnumShopcartSortAttribute.java
+++ src/main/java/com/airton/psjava/enums/EnumShopcartSortAttribute.java
@@ -1,2 +1,28 @@
-package com.airton.psjava.enums;public enum EnumShopcartSortAttribute {
+package com.airton.psjava.enums;
+
+public enum EnumShopcartSortAttribute {
+
+    PRICE("PRICE"),
+    SCORE("SCORE"),
+    NAME("NAME");
+
+    private String sortAttribute;
+
+    EnumShopcartSortAttribute(String sortAttribute) {
+        this.sortAttribute = sortAttribute;
+    }
+
+    public static EnumShopcartSortAttribute valueOfString(String sortAttribute) {
+        for (EnumShopcartSortAttribute item : EnumShopcartSortAttribute.values()) {
+            if (item.getSortAttribute().equals(sortAttribute)) {
+                return item;
+            }
+        }
+        return null;
+    }
+
+    public String getSortAttribute() {
+        return sortAttribute;
+    }
+
 }
