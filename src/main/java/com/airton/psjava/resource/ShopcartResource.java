4f73698f05b6f3a356b34be744e76c35bed960f3
airtonsm <airtonsmorais28@gmail.com>
2021-06-21 19:35:19 -0300

    checkout shopcarts

--- src/main/java/com/airton/psjava/resource/ShopcartResource.java
+++ src/main/java/com/airton/psjava/resource/ShopcartResource.java
@@ -5,10 +5,7 @@ import com.airton.psjava.service.ShopcartService;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -23,6 +20,18 @@ public class ShopcartResource {
     public ResponseEntity<List<ShopcartDTO>> findAll(@RequestParam(required = false) String sortAttribute) {
         List<ShopcartDTO> list = service.findAll(StringUtils.upperCase(sortAttribute));
         return ResponseEntity.ok().body(list);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<ShopcartDTO> findById(@PathVariable Long id, @RequestParam(required = false) String sortAttribute) {
+        ShopcartDTO obj = service.findById(id, StringUtils.upperCase(sortAttribute));
+        return ResponseEntity.ok().body(obj);
+    }
+
+    @GetMapping(value = "/checkout/{id}")
+    public ResponseEntity<ShopcartDTO> checkout(@PathVariable Long id) {
+        ShopcartDTO obj = service.checkout(id);
+        return ResponseEntity.ok().body(obj);
     }
 
 }
