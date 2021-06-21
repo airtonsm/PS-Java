1835a91f09196f4024f19f4b400907b3f64ebd7e
airtonsm <airtonsmorais28@gmail.com>
2021-06-21 19:40:14 -0300

    dto for insert products in shopcart

---
+++ src/main/java/com/airton/psjava/dto/ProductQuantityDTO.java
@@ -0,0 +1,29 @@
+package com.airton.psjava.dto;
+
+public class ProductQuantityDTO {
+
+    private Long id;
+    private Integer quantity;
+
+    public ProductQuantityDTO(Long id, Integer quantity) {
+        this.id = id;
+        this.quantity = quantity;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+}
