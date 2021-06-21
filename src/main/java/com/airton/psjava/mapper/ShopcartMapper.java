ba1c1570514919eb4c9d7de432a72d0e38513388
airtonsm <airtonsmorais28@gmail.com>
2021-06-21 19:05:42 -0300

    return dto

--- src/main/java/com/airton/psjava/mapper/ShopcartMapper.java
+++ src/main/java/com/airton/psjava/mapper/ShopcartMapper.java
@@ -1,6 +1,31 @@
 package com.airton.psjava.mapper;
 
+import com.airton.psjava.dto.ProductDTO;
+import com.airton.psjava.dto.ShopcartDTO;
+import com.airton.psjava.entities.Shopcart;
+import com.airton.psjava.entities.ShopcartProduct;
+import org.apache.commons.lang3.StringUtils;
+
 public class ShopcartMapper {
+
+    public static ShopcartDTO toDTO(Shopcart shopcart, String sortAttribute) {
+        ShopcartDTO dto = new ShopcartDTO();
+
+        dto.setId(shopcart.getId());
+        dto.setMoment(shopcart.getMoment());
+
+        for (ShopcartProduct sp : shopcart.getProducts()) {
+            ProductDTO pdto = ProductMapper.toDTO(sp.getProduct());
+            pdto.setQuantity(sp.getQuantity());
+            dto.setProductQuantity(dto.getProductQuantity() + sp.getQuantity());
+            dto.getProducts().add(pdto);
+        }
+
+        if (StringUtils.isNotBlank(sortAttribute)) {
+            dto.sortProducts(sortAttribute);
+        }
+        return dto;
+    }
 
 
 
