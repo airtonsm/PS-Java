0e5eb4b95c3e0dd92a68544d299e81b33b7b942b
airtonsm <airtonsmorais28@gmail.com>
2021-06-21 19:22:10 -0300

    findByid(per param)

--- src/main/java/com/airton/psjava/service/ShopcartService.java
+++ src/main/java/com/airton/psjava/service/ShopcartService.java
@@ -1,6 +1,8 @@
 package com.airton.psjava.service;
 
 import com.airton.psjava.dto.ShopcartDTO;
+import com.airton.psjava.entities.Shopcart;
+import com.airton.psjava.exception.ResourcesNotFoundException;
 import com.airton.psjava.mapper.ShopcartMapper;
 import com.airton.psjava.repository.ShopcartRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,6 +18,11 @@ public class ShopcartService {
 
     public List<ShopcartDTO> findAll(String sortAttribute) {
         return ShopcartMapper.toDTOList(shopcartRepository.findAll(), sortAttribute);
+    }
+
+    public ShopcartDTO findById(Long id, String sortAttribute) {
+        Shopcart shopcart = shopcartRepository.findById(id).orElseThrow(() -> new ResourcesNotFoundException(id));
+        return ShopcartMapper.toDTO(shopcart, sortAttribute);
     }
 
 }
