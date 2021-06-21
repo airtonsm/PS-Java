e84f0d8cd5f590c6a95214cfc8783a68465c6584
airtonsm <airtonsmorais28@gmail.com>
2021-06-21 19:15:19 -0300

    created service and findall per ordering

--- src/main/java/com/airton/psjava/service/ShopcartService.java
+++ src/main/java/com/airton/psjava/service/ShopcartService.java
@@ -1,2 +1,21 @@
-package com.airton.psjava.service;public class ShopcartService {
+package com.airton.psjava.service;
+
+import com.airton.psjava.dto.ShopcartDTO;
+import com.airton.psjava.mapper.ShopcartMapper;
+import com.airton.psjava.repository.ShopcartRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ShopcartService {
+
+    @Autowired
+    private ShopcartRepository shopcartRepository;
+
+    public List<ShopcartDTO> findAll(String sortAttribute) {
+        return ShopcartMapper.toDTOList(shopcartRepository.findAll(), sortAttribute);
+    }
+
 }
