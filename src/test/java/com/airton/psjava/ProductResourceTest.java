c9177bb0ec81804805ec6ed0aac8258f6bd55297
airtonsm <airtonsmorais28@gmail.com>
2021-06-21 20:13:07 -0300

    test delete product

--- src/test/java/com/airton/psjava/ProductResourceTest.java
+++ src/test/java/com/airton/psjava/ProductResourceTest.java
@@ -1,13 +1,61 @@
 package com.airton.psjava;
 
+import com.airton.psjava.dto.ProductDTO;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.jayway.jsonpath.JsonPath;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 
-@SpringBootTest
-class PsJavaApplicationTests {
+import java.math.BigDecimal;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, classes = PsJavaApplication.class)
+@AutoConfigureMockMvc
+@EnableAutoConfiguration(exclude = SecurityAutoConfiguration.class)
+@AutoConfigureTestDatabase
+class ProductResourceTest {
+
+    private final static String BASE_URL = "/products/";
+
+    @Autowired
+    private MockMvc mvc;
 
     @Test
-    void contextLoads() {
+    public void Delete_Product_Resource() throws Exception {
+
+        ProductDTO product = getExampleProduct();
+
+        MvcResult result = mvc.perform(post(BASE_URL)
+                .content(new ObjectMapper().writeValueAsString(product))
+                .contentType(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isCreated())
+                .andReturn();
+
+        Integer id = JsonPath.read(result.getResponse().getContentAsString(), "$.id");
+
+        mvc.perform(delete(BASE_URL + id).contentType(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isNoContent());
     }
 
+
+    private ProductDTO getExampleProduct() {
+        return new ProductDTO(null, "PES2021", BigDecimal.valueOf(199.00), Short.parseShort("170"), "pes2021.png");
+    }
 }
